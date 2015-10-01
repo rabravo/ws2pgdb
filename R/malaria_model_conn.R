2ed65a10fe774bab631da7542d30c66602a6b234
rabravo <angel.bravo@unt.edu>
2015-10-01 17:03:21 -0500

    Remove disease_model function definitions from stretch_delay_latent_period function

---
+++ R/malaria_model_conn.R
@@ -0,0 +1,57 @@
+#' malaria_model_conn() habia una vez ...
+#'
+#' @param tableData is the name of the table containing weather data ( temperature )
+#' @param disease is the name of the vector-borne disease (i.e dengue, malaria, chikungunya, etc)
+#' @param conn is an open connectino to read/write to a pgdb
+#' @param degree is the order at one wants to adjusts the polynomio (for dengue degree=5) has the lowest norm
+#' @return returns the tail of the data that has been found. 
+#' @examples
+#' file   <- base::paste(Sys.getenv("HOME"), "/","pg_config.yml", sep="")
+#' config <- yaml::yaml.load_file( file )
+#' driver <- "PostgreSQL"
+#' drv    <- RPostgres::Postgres()
+#' conn   <- RPostgres::dbConnect(drv, host= config$dbhost, port= config$dbport, dbname= config$dbname, user= config$dbuser, password= config$dbpwd)
+#' ghcnd <- 'GHCND'
+#' geoid <- '12087'
+#' type  <- 'TMAX'
+#' disease <- 'malaria'
+#' degree  <- '5'
+#' stations <- as.data.frame( all_coor_ws( ghcnd, geoid, type) )
+#' span <- '2'
+#' tableData <- ws_metadata_span_2_pgdb( geoid, type, stations, span) 
+#' tableDisease <- base::paste(tableData,"_malaria",sep="") 
+#' q    <- base::paste("select r_table_exists('", tableDisease,"')", sep="")
+#' res   <- RPostgres::dbSendQuery(conn, q)
+#' exists <- RPostgres::dbFetch(res)
+#' RPostgres::dbClearResult(res)
+#' if( as.integer( exists) ){ print("Exists!") }else{ malaria_model_conn(tableData, disease, conn, degree) }
+#' RPostgres::dbDisconnect(conn)
+#' @export
+malaria_model_conn <- function(tableData, disease, conn, degree){
+  library(RcppOctave)
+  q1    <- base::paste("SELECT * FROM ", tableData, sep="")
+  res   <- RPostgres::dbSendQuery(conn, q1)
+  ws_data <- data.frame(RPostgres::dbFetch(res), row.names=NULL)
+  ws_data_temp <- data.matrix(ws_data, rownames.force=NA)
+  RPostgres::dbClearResult(res)
+  .O$real_temp <- ws_data_temp
+  if(1){
+    #.O$num <- degree
+    RcppOctave::o_source(text="\
+	      malaria_m = vectorize(inline(char('0.71 * (T - 21 ) + 4.7')));\
+              for j=1:columns(real_temp)\
+                for k=1:rows(real_temp)\
+                  if (real_temp(k,j) > 20 && real_temp(k,j) <= 31)\
+                    eip(k,j) = malaria_m( real_temp(k,j) );\
+                  else\
+                    eip(k,j) = 0;\
+                  endif\
+                endfor\
+             endfor\
+	    ")
+  }
+  tableName <- paste(tableData,"_",disease, sep="")
+  RPostgres::dbWriteTable( conn, tableName, as.data.frame( .O$eip ) )
+  o_clear(all=TRUE)
+  return(tableName)
+}
