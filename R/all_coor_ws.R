a92aca823b00f61781f10f0a1ae92ad65ec0f624
Angel Bravo <angel.bravo@unt.edu>
2026-04-16 18:45:11 -0400

    Revert "Replace config$token with Sys.getenv("NOAA_TOKEN") in all_coor_ws.R."
    This reverts commit 6fc0ba199cb041feb8fe6c481e87f85ea3d46135.

--- R/all_coor_ws.R
+++ R/all_coor_ws.R
@@ -1,7 +1,7 @@
-#' all_coor_ws() retrieve weather station within a region ( State/County )
+#' all_coor_ws() retrieve weather station within a region ( State/County )  
 #'
 #' The function retrieves all NOAA weather station (ws) data available in a region ( State/County ) related
-#' to the weather variable under investigation (type).
+#' to the weather variable under investigation (type).  
 #'
 #' @keywords NOAA, weather stations, rnoaa
 #' @param ghcnd  String that refers to the Global Historical Climate Network (daily) dataset
@@ -16,26 +16,38 @@
 #' @export
 all_coor_ws <- function( ghcnd, geoid, type) {
 
-  token <- Sys.getenv("NOAA_TOKEN")
-  FIPS  <- base::paste("FIPS:", geoid, sep = "")
+  tryCatch(
+    {
+      file    <- NULL
+      path    <- base::paste(Sys.getenv("HOME"), "/", "pg_config.yml", sep = "")
+      file    <- yaml::yaml.load_file(path)
+      FIPS    <- base::paste("FIPS:", geoid, sep = "")    
+    },error = function (err_msg){  
+        message(base::paste("msg: ", err_msg, sep=""))
+        return(file)
+    },warning = function (warn_msg){
+        message(base::paste("msg: ", warn_msg, sep=""))
+        return(file)
+    }
+  )
 
   # Check whether NOAA site returned any data
   tryCatch(
     {
-      ws       <- NULL
+      ws <- NULL
       stations <- NULL
-      ws       <- rnoaa::ncdc_stations(datasetid=ghcnd, datatypeid=type, locationid=FIPS, limit=1000, token=token)
-      stations <- ws$data
-    },error = function (err_msg){
+      ws      <- rnoaa::ncdc_stations(datasetid=ghcnd, datatypeid=type, locationid=FIPS, limit=1000, token=config$token) 
+      stations<- ws$data
+    },error = function (err_msg){  
         message(base::paste("msg: ", err_msg, sep=""))
         return(stations)
     },warning = function (warn_msg){
-        message(base::paste("msg: ", warn_msg, sep=""))
+        message(base::paste("msg: ", err_msg, sep=""))
         return(stations)
     }
-  )
-
-  # Handle special case for FIPS 48113 where stations have identical data
+  )  
+  
+  # Handle special case for FIPS 12087 where stations have identical data (?)
   if(geoid == '48113'){
       switch(type,
           PRCP = stations <- subset(stations, id != "GHCND:USW00013907" & id != "GHCND:USW00093928"),
@@ -44,7 +56,7 @@ all_coor_ws <- function( ghcnd, geoid, type) {
       )
   }
   prefix <- base::paste(ghcnd, ":", sep="")
-  stations$id <- gsub(prefix, "", stations$id)
+  stations$id <- gsub(prefix, "", stations$id) 
   return(stations)
 
 }#endFUNCTION
